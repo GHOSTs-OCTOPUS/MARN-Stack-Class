518e6c9dea7291eb287306831580bf4f77a90ac4
GHOSTs-OCTOPUS <vikaskole786@gmail.com>
2026-05-27 13:05:18 +0530

    React Components - Functional, Class

--- React/demo-app/src/App.jsx
+++ React/demo-app/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import MyFunction from './Myfunction'
+import Myclass from './Myclass'
+const App = () => (
+  <div>
+    <hr />
+     <h1>Hello, React!</h1>
+        <p>Welcome to your React application.</p>
+        <button onClick={() => alert('Button clicked!')}>Click Me</button>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+        
+    <hr />
+    <MyFunction />
+    <Myclass />
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-      <p>Welcome to your React application.</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
-    </div>
-  )
-}
+  </div>
+)
 
 export default App
 
---
+++ React/demo-app/src/Myfunction.jsx
@@ -0,0 +1,18 @@
+// Myfunction.jsx For import that use "rafce"
+
+import React from 'react'
+
+const Myfunction = () => {
+  return (
+    <div>
+      <hr />
+      <h1>My Function Component</h1>
+      <p>This is a function component.</p>
+      <hr />
+      
+    </div>
+  )
+}
+
+export default Myfunction
+
