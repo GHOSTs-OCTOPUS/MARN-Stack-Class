a46234774522cee11061b467005c81188582719e
GHOSTs-OCTOPUS <vikaskole786@gmail.com>
2026-06-08 13:03:20 +0530

    demo app delate

--- React/demo-app/src/component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className="footer">
-      <p>&copy; 2023 My Website. All rights reserved.</p>
-    </div>
-  )
-}
