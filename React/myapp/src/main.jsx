d33f2db2b2ab4aa060f24b7284dca7790115fc8a
GHOSTs-OCTOPUS <vikaskole786@gmail.com>
2026-07-09 13:43:22 +0530

    API Call

--- React/myapp/src/main.jsx
+++ React/myapp/src/main.jsx
@@ -9,6 +9,7 @@ import Myapi from './Myapi'
 createRoot(document.getElementById('root')).render(
   <>
   <App/>
+  
   {/* <Myclassprops id={101} name="raj"/> */}
 
   {/* <Myone id={1} name="sahil"/> */}
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
   <br />
   <hr />
   <Myapi/>
+  
   
   </>
 )
